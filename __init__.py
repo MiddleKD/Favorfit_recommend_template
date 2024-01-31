1526cd0d2a48ec341c21b9f3a311d0eec10ef0f1
ml-favorfit-develop <122414946+MiddleKD@users.noreply.github.com>
2024-01-31 18:10:40 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,4 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "similar_color_model"))
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "creative_color_model"))
